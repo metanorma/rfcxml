2801cbf78b365efd2057acc91126b0b21ba6fe44
Ronald Tse <ronald.tse@ribose.com>
2026-04-20 17:54:51 +0800

    fix: add collection: true to mixed content attributes
    Mixed content with map_content requires the content attribute to be a
    string collection to properly handle multiple interleaved text nodes.
    
    Required by lutaml-model MixedContentCollectionError validation.

--- lib/rfcxml/v3/li.rb
+++ lib/rfcxml/v3/li.rb
@@ -5,7 +5,7 @@ require "lutaml/model"
 module Rfcxml
   module V3
     class Li < Lutaml::Model::Serializable
-      attribute :content, :string
+      attribute :content, :string, collection: true
       attribute :anchor, :string
       attribute :pn, :string
       attribute :artset, Artset, collection: true
